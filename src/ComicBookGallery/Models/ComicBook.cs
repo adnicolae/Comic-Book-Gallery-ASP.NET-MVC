6ee50ec82509150d6fe8b7253cb1941a4afbb437
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-09-25 22:40:14 +0300

    Update Comic Book Model and Repository

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,7 +8,7 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public Series Series { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
@@ -18,7 +18,16 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle + " #" + IssueNumber;
+                // check if series is null before accessing Title
+                var series = Series;
+                if (series != null)
+                {
+                    return Series.Title + " #" + IssueNumber;
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
 
@@ -27,7 +36,15 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                var series = Series;
+                if (series != null)
+                {
+                    return Series.Title.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                } 
+                else
+                {
+                    return null;
+                }
             }
         }
     }
--- src/ComicBookGallery/Models/Series.cs
+++ src/ComicBookGallery/Models/Series.cs
@@ -8,7 +8,7 @@ namespace ComicBookGallery.Models
     public class Series
     {
         public int Id { get; set; }
-        public string Title { get; set }
+        public string Title { get; set; }
         public string DescriptionHtml { get; set; }
         public ComicBook[] Issues { get; set; }
     }
